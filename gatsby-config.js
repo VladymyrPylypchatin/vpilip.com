e61c31b556b92de7665c8525700946587e108cb5
Vova Pilipchatin <vladymyr.pylypchatin@gmail.com>
2020-01-25 11:14:10 +0200

    deleted extra plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
-
+console.log('Envivroment', process.env.NODE_ENV);
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -10,27 +10,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+
     `gatsby-plugin-sass`,
     // {
     //   resolve: `gatsby-source-prismic`,
