f92722d9feb33658989d9d944d95b7d1fcee9933
Vova Pilipchatin <vladymyr.pylypchatin@gmail.com>
2020-02-13 14:22:56 +0200

    add trailing slash

--- src/components/BlogPage/PostCard/PostCard.jsx
+++ src/components/BlogPage/PostCard/PostCard.jsx
@@ -6,10 +6,10 @@ import styles from './PostCard.module.scss';
 const PostCard = (props) => {
     return (
         <article className={styles.postCard}>
-            <Link to={`/${props.uid}`} className={styles.postCard__preview}>
+            <Link to={`/${props.uid}/`} className={styles.postCard__preview}>
                 <img src={props.preview} alt={props.title} />
             </Link>
-            <Link to={`/${props.uid}`} className={styles.postCard__body}>
+            <Link to={`/${props.uid}/`} className={styles.postCard__body}>
                 <div className={styles.postCard__tag}>{props.tag}</div>
                 <h2 className={styles.postCard__title}>{props.title}</h2>
                 <h4>
