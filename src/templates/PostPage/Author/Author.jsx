dfd5f974b731e20da1e7b28fa1ed58fe39c96e66
Vova Pilipchatin <vladymyr.pylypchatin@gmail.com>
2020-05-14 17:16:26 +0300

    added author of the post block

---
+++ src/templates/PostPage/Author/Author.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './Author.module.scss';
+import { Link } from 'gatsby'
+
+
+const Author = (props) => {
+    console.log(props);
+    const links = props.links.filter(l => l.url).map((linkObject) => {
+        return (
+            <a
+                key={linkObject.id}
+                href={linkObject.url}
+                target='_blank'
+                rel="noopener noreferrer"
+                className={styles.link}
+            >
+                <img src={linkObject.img} alt="social link" />
+            </a>
+        );
+    });
+    return (
+        <div className={styles.authorBlock}>
+            <div>Article by:</div>
+            <div className={styles.author}>
+                <Link href={'/'} className={styles.author__avatar}>
+                    <img src={props.profileImg} alt={props.name} />
+                </Link>
+                <div className={styles.author__info}>
+                    <div className={styles.author__name}><Link href={'/'}>{props.name}</Link></div>
+                    <div className={styles.author__description}>
+                        {props.description}
+                    </div>
+                    <div className={styles.author__socials}>
+                        {links}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Author;
--- src/templates/PostPage/PostPage.jsx
+++ src/templates/PostPage/PostPage.jsx
@@ -7,10 +7,18 @@ import SEO from '../../components/seo';
 import PostBody from './PostBody/PostBody'
 import PostHeader from './PostHeader/PostHeader';
 import EmailSubs from '../../components/EmailSubs/EmailSubs';
+import Author from './Author/Author';
+
+import HomeIcon from '../../assets/img/author/home.svg';
+import TwitterIcon from '../../assets/img/author/twitter.svg';
+import InstagramIcon from '../../assets/img/author/instagram.svg';
+import FacebookIcon from '../../assets/img/author/facebook.svg';
+import LinkedinIcon from '../../assets/img/author/linkedin.svg';
 
 const PostPage = ({ data }) => {
     console.log(data);
     const blogPost = data.prismic.blogpost;
+    const author = blogPost.author;
     return (
         <PostLayout >
             <SEO
@@ -27,6 +35,24 @@ const PostPage = ({ data }) => {
                 <div className={['post-container', styles.subscriberForm].join(' ')}>
                     <EmailSubs formId={'1383803'} />
                 </div>
+                {
+                    author ?
+                        <div className={['post-container', styles.author].join(' ')}>
+                            <Author
+                                name={author.author_name[0].text}
+                                description={author.author_description[0].text}
+                                profileImg={author.author_profile_img.Preview.url}
+                                links={[
+                                    { id: 'main', url: author.main_website_link?.url, img: HomeIcon },
+                                    { id: 'linkedin', url: author.linkedin_link?.url, img: LinkedinIcon },
+                                    { id: 'twitter', url: author.twitter_link?.url, img: TwitterIcon },
+                                    { id: 'instagram', url: author.instagram_link?.url, img: InstagramIcon },
+                                    { id: 'facebook', url: author.facebook_link?.url, img: FacebookIcon },
+                                ]}
+                            />
+                        </div>
+                        : null
+                }
             </div>
         </PostLayout>
     );
@@ -42,6 +68,38 @@ export const query = graphql`
             post_title
             sub_title
             cover
+            author {
+                ... on PRISMIC_Author {
+                    author_name
+                    author_description
+                    author_profile_img
+                    facebook_link {
+                      ... on PRISMIC__ExternalLink {
+                        url
+                      }
+                    }
+                    instagram_link {
+                      ... on PRISMIC__ExternalLink {
+                        url
+                      }
+                    }
+                    linkedin_link {
+                      ... on PRISMIC__ExternalLink {
+                        url
+                      }
+                    }
+                    main_website_link {
+                      ... on PRISMIC__ExternalLink {
+                        url
+                      }
+                    }
+                    twitter_link {
+                      ... on PRISMIC__ExternalLink {
+                        url
+                      }
+                    }
+                  }
+            }
           }
         } 
     }  
