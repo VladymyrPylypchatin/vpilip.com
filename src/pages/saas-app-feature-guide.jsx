568923aec54565b8d7595eebced892a06e68008a
Vladimir Pilipchatin <vladymyr.pylypchatin@gmail.com>
2020-08-15 14:35:35 +0300

    updated seo

--- src/pages/saas-app-feature-guide.jsx
+++ src/pages/saas-app-feature-guide.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import FeatureGuideTemplate from '../templates/FeatureGuideTemplate/FeatureGuideTemplate';
+import SEO from '../components/seo';
+
+import PreviewImg from '../assets/img/resources/featureguide/preview.png';
 
 const SaasGuidePage = () => {
     return (
-        <FeatureGuideTemplate />
+        <>
+            <SEO
+                title="The non-tech founder's SaaS App Feature Guide by Vova Pilipchatin"
+                customTitle
+                meta={[
+                    {
+                        name: `og:image`,
+                        content: PreviewImg,
+                    },
+                ]}
+                description="Shape up your SaaS idea and define its features to make your future app aligned with your vision, business goals, and budget."
+            />
+            <FeatureGuideTemplate />
+        </>
     );
 };
 
