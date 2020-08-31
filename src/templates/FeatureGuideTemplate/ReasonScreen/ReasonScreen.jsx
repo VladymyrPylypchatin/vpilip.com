30f8367ad9ecffe03d41e779e27b267711ee05c6
Vladimir Pilipchatin <vladymyr.pylypchatin@gmail.com>
2020-08-31 09:03:39 +0300

    updated formating

--- src/templates/FeatureGuideTemplate/ReasonScreen/ReasonScreen.jsx
+++ src/templates/FeatureGuideTemplate/ReasonScreen/ReasonScreen.jsx
@@ -16,7 +16,7 @@ const ReasonScreen = () => {
                             Developers and designers can build anything. The question is, does it do what you need.
                         </p>
                         <p>
-                            👉 You are the one who best understands the problem and solution, but your ability to communicate your vision to designers and developers will ultimately determine your product's success.
+                            👉 You are the one who best understands the problem and solution, but <b>your ability to communicate your vision</b> to designers and developers will ultimately <b>determine your product's success.</b>
                         </p>
                         <p>
                             Well-defined features and architecture of SaaS Application will help you save time, money, and deliver a better product.
